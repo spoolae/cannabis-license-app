40ae6df39fe1b29a752a8110053ff8f6890b9466
Spoolae <ohotnic147feed@gmail.com>
2023-12-10 12:40:52 +0100

    Added User Authenticate

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -91,4 +91,64 @@ router.post("/create-user", async (req, res) => {
   }
 });
 
+router.post("/authenticate", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: "Invalid input" });
+    }
+
+    const client = await pool.connect();
+
+    try {
+      // Находим пользователя по email
+      const userResult = await client.query(
+        "SELECT * FROM users WHERE email = $1",
+        [email]
+      );
+
+      if (userResult.rows.length === 0) {
+        return res.status(401).json({ error: "Invalid email or password" });
+      }
+
+      const user = userResult.rows[0];
+
+      // Сравниваем введенный пароль с хешированным паролем в базе данных
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return res.status(401).json({ error: "Invalid email or password" });
+      }
+
+      // Если пароль совпадает, возвращаем полную информацию о пользователе
+      if (user.role === "medic") {
+        const medicResult = await client.query(
+          "SELECT * FROM medics WHERE user_id = $1",
+          [user.user_id]
+        );
+        const medic = medicResult.rows[0];
+        return res.json({ user, medic });
+      } else if (user.role === "patient") {
+        const patientResult = await client.query(
+          "SELECT * FROM patients WHERE user_id = $1",
+          [user.user_id]
+        );
+        const patient = patientResult.rows[0];
+        return res.json({ user, patient });
+      } else {
+        return res.json({ user });
+      }
+    } catch (error) {
+      console.error("Error authenticating user", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+      client.release();
+    }
+  } catch (error) {
+    console.error("Error parsing request body", error);
+    res.status(400).json({ error: "Bad Request" });
+  }
+});
+
 module.exports = router;
