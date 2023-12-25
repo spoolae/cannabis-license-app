1eee9a671be477ca4b954718e99aed53335f342b
Spoolae <ohotnic147feed@gmail.com>
2023-12-25 16:16:23 +0100

    Added Header

--- client/src/components/MedicHeader.jsx
+++ client/src/components/MedicHeader.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const MedicHeader = () => {
   return (
     <div className="header">
-      <h2>Hi, Doctor Johnson</h2>
+      <h2 className="header-title">
+        <Link to="/">
+          Hi, <span>Doctor Johnson</span>
+        </Link>
+        <FontAwesomeIcon icon={faPen} className="edit-icon" />
+      </h2>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" activeClassName="active">
+              Licenses
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/" activeClassName="active">
+              Patients
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/" activeClassName="active">
+              Medications
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
