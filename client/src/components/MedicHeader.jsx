d10423ef2f070461e7d6a008f2cb8fc7e6a0a380
Spoolae <ohotnic147feed@gmail.com>
2024-01-15 13:13:34 +0100

    Fixed Header

--- client/src/components/MedicHeader.jsx
+++ client/src/components/MedicHeader.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useSelector } from "react-redux";
 
 export const MedicHeader = () => {
+  const user = useSelector((state) => state.auth.user);
+
+  console.log("User:", user);
+
+  const greetingText = user?.user.last_name
+    ? `Doctor ${user.user.last_name}`
+    : user?.user.email || "You're welcome";
+
   return (
     <div className="header">
       <h2 className="header-title">
         <Link to="/medic/profile">
-          Hi, <span>Doctor Johnson</span>
+          Hi, <span>{greetingText}</span>
         </Link>
         <FontAwesomeIcon icon={faPen} className="edit-icon" />
       </h2>
