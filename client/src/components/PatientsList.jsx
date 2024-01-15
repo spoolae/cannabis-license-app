cb7de433464e448bc8da163002d618bac0252dad
Spoolae <ohotnic147feed@gmail.com>
2024-01-15 12:42:30 +0100

    Improved Styles

--- client/src/components/PatientsList.jsx
+++ client/src/components/PatientsList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPatients, deletePatient } from "../redux/patientsSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMars, faVenus } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faMars, faVenus } from "@fortawesome/free-solid-svg-icons";
 import defaultAvatar from "../assets/images/avatar.png";
 
 export const PatientsList = () => {
@@ -18,8 +18,8 @@ export const PatientsList = () => {
   };
 
   return (
-    <div className="medications-list">
-      <h3 className="title">Medications List</h3>
+    <div className="patients-list">
+      <h3 className="title">Patients List</h3>
       <div className="list-content">
         <ul>
           {patients.map((patient) => (
@@ -43,10 +43,26 @@ export const PatientsList = () => {
                   <p>{`${patient.first_name} ${patient.father_name}`}</p>
                 </div>
                 <div>
-                  <p>{patient.health_description}</p>
+                  <h5>
+                    {patient.license_number
+                      ? patient.license_number
+                      : "Invalid License"}
+                  </h5>
+                  <p>
+                    {patient.health_description
+                      ? patient.health_description
+                      : "There's no health info"}
+                  </p>
                 </div>
+
                 <div>
-                  <h5>{patient.license_number}</h5>
+                  <p>
+                    <FontAwesomeIcon
+                      icon={faEnvelope}
+                      style={{ marginRight: "5px" }}
+                    />
+                    Contact:
+                  </p>
                   <p>{patient.email}</p>
                 </div>
               </div>
